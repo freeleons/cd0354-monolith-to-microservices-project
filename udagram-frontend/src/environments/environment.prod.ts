787418a95b9a63a21859b48609d068f9567583dc
Jue Wang's Github <freeleons@gmail.com>
2022-09-03 04:13:28 -0400

    added deployment and service yaml files

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a87fc4c70b3c54c8fb0166b59191b20f-199070281.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
